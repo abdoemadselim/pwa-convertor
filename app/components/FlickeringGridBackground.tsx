d8d28a48f414c2ecb847cf0919aab869c1acf1c6
abdoemadselim <abdoemadselim11@gmail.com>
2025-01-29 20:47:26 +0200

    adding animated background

--- app/components/FlickeringGridBackground.tsx
+++ app/components/FlickeringGridBackground.tsx
@@ -3,12 +3,12 @@ import { FlickeringGrid } from "@/components/ui/flickering-grid";
 export function FlickeringGridBackground() {
   return (
       <FlickeringGrid
-        className="fixed inset-0 z-0  size-full"
-        squareSize={4}
+        className="fixed inset-0 z-0 size-full"
+        squareSize={6}
         gridGap={6}
         color="#6B7280"
-        maxOpacity={0.5}
-        flickerChance={0.1}
+        maxOpacity={0.3}
+        flickerChance={0.05}
       />
   );
 }
